c483cd170720dfbe9b0d1cc5c0eeb39ed4be899d
Andre Mata <=>
2020-09-16 21:51:15 -0300

    project created

---
+++ backend/src/main/java/com/andremata/apppesquisa/AppPesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.andremata.apppesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/andremata/apppesquisa/AppPesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andremata.apppesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppPesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
