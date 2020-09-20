89d9f67e14f2ed9ef7eb598226906f6aa4bb8ef6
Andre Mata <=>
2020-09-20 19:10:55 -0300

    Adicionando a pasta front-mobile

---
+++ front-mobile/src/pages/CreateRecord/index.tsx
@@ -0,0 +1,210 @@
+import React, { useState, useEffect } from 'react';
+import { FontAwesome5 as Icon } from '@expo/vector-icons';
+import { StyleSheet, View, TextInput, Text, Alert } from 'react-native';
+import Header from '../../components/Header';
+import PlatformCard from './PlatformCard';
+import { GamePlatform, Game } from './types';
+import RNPickerSelect from 'react-native-picker-select';
+import { RectButton } from 'react-native-gesture-handler';
+import axios from 'axios';
+
+const placeholder = {
+    label: "Selecione o game",
+    value: null,
+}
+
+const BASE_URL = 'http://192.168.1.105:8080';
+
+const mapSelectValues = (games: Game[]) => {
+    return games.map(game => ({
+        ...game,
+        label: game.title,
+        value: game.id
+    }));
+}
+
+const CreateRecord = () => {
+    //cria um estado para o componente
+    const [name, setName] = useState('');
+    const [age, setAge] = useState('');
+    const [platform, setPlatform] = useState<GamePlatform>();
+    const [selectedGame, setSeletecdGame] = useState('');
+    const [allGame, setAllGame] = useState<Game[]>([]);
+    const [filteredGame, setFilteredGame] = useState<Game[]>([]);
+
+    const handleChangePlatform = (selectPlatform: GamePlatform) => {
+        setPlatform(selectPlatform);
+        const gamesByPlatform = allGame.filter(
+            game => game.platform ===selectPlatform
+        )
+        setFilteredGame(gamesByPlatform);
+    }
+
+    const handleSubit = () => {
+        const payload = {name, age, gameId: selectedGame};
+
+        axios.post(`${BASE_URL}/records`, payload)
+        .then(() => {
+            Alert.alert('Salvo com sucesso!');
+            setName('');
+            setAge('');
+            setSeletecdGame('');
+            setPlatform(undefined);
+        })
+        .catch(() => Alert.alert('Erro ao salvar!'))
+    }
+
+    //ve o ciclo de vida do componente
+    //quando inicia o componente executa esse código
+    useEffect(() => {
+        axios.get(`${BASE_URL}/games`)
+        .then(response =>{
+            const selectValue = mapSelectValues(response.data);
+            setAllGame(selectValue);
+        })
+        .catch(() => Alert.alert('Erro ao carregar games!'))
+    }, []);
+
+    return (
+        <>
+        <Header />
+        
+        <View style={styles.container}>
+            <TextInput 
+                style={styles.inputText} 
+                placeholder="Nome" 
+                placeholderTextColor="#9E9E9E"
+                onChangeText={text => setName(text)}
+                value={name}/>
+            <TextInput 
+                keyboardType="numeric"
+                style={styles.inputText} 
+                placeholder="Idade" 
+                placeholderTextColor="#9E9E9E"
+                maxLength={2}
+                onChangeText={age => setAge(age)}
+                value={age}/>
+
+            <View style={styles.platformContainer}>
+                <PlatformCard 
+                    platform='PC' 
+                    icon="laptop" 
+                    onChange={handleChangePlatform}
+                    activePlatform={platform}/>
+
+                <PlatformCard 
+                    platform='XBOX' 
+                    icon="xbox" 
+                    onChange={handleChangePlatform}
+                    activePlatform={platform}/>
+
+                <PlatformCard 
+                    platform='PLAYSTATION' 
+                    icon="playstation" 
+                    onChange={handleChangePlatform}
+                    activePlatform={platform}/>
+            </View>
+
+            <RNPickerSelect
+                onValueChange={value => {
+                    setSeletecdGame(value);
+                }}
+                placeholder={placeholder}
+                value={selectedGame}
+                items={filteredGame}
+                style={pickerSelectStyles}
+                Icon={() => {
+                    return <Icon name="chevron-down" color="#9E9E9E" size={25}/>
+                }}
+            />
+
+            <View style={styles.footer}>
+                <RectButton style={styles.button} onPress={handleSubit}>
+                    <Text style={styles.buttonText}>
+                        SALVAR
+                    </Text>
+                </RectButton>
+            </View>
+        </View>
+        </>
+    );
+};
+
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+      fontSize: 16,
+      paddingVertical: 12,
+      paddingHorizontal: 20,
+      backgroundColor: '#FFF',
+      borderRadius: 10,
+      color: '#ED7947',
+      paddingRight: 30,
+      fontFamily: "Play_700Bold",
+      height: 50
+    },
+    inputAndroid: {
+      fontSize: 16,
+      paddingVertical: 12,
+      paddingHorizontal: 20,
+      backgroundColor: '#FFF',
+      borderRadius: 10,
+      color: '#ED7947',
+      paddingRight: 30,
+      fontFamily: "Play_700Bold",
+      height: 50
+    },
+    placeholder: {
+      color: '#9E9E9E',
+      fontSize: 16,
+      fontFamily: "Play_700Bold",
+    },
+    iconContainer: {
+      top: 10,
+      right: 12,
+    }
+})
+
+const styles = StyleSheet.create({
+    container: {
+      marginTop: '15%',
+      paddingRight: '5%',
+      paddingLeft: '5%',
+      paddingBottom: 50
+    },
+    inputText: {
+      height: 50,
+      backgroundColor: '#FFF',
+      borderRadius: 10,
+      color: '#ED7947',
+      fontFamily: "Play_700Bold",
+      fontSize: 16,
+      paddingLeft: 20,
+      marginBottom: 21
+    },
+    platformContainer: {
+      marginBottom: 20,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    footer: {
+      marginTop: '15%',
+      alignItems: 'center',
+    },
+    button: {
+      backgroundColor: '#00D4FF',
+      flexDirection: 'row',
+      borderRadius: 10,
+      height: 60,
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    buttonText: {
+      fontFamily: "Play_700Bold",
+      fontWeight: 'bold',
+      fontSize: 18,
+      color: '#0B1F34',
+    }
+  });
+
+export default CreateRecord;
